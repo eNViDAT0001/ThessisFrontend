b65b34966022db7ddf5dd8a77fe6a21d98889bb4
eNViDAT <ngvidat@gmail.com>
2023-07-25 01:16:54 +0700

    x

--- src/config/index.js
+++ src/config/index.js
@@ -6,7 +6,6 @@ export const API_BASE_URL_WEBSOCKET = "ws://localhost:8082/api/v1/";
 
 const baseURL = host => `http://${host}:${PORT}/api/v1`
 const baseWebsocket = host => `ws://${host}:${PORT}/api/v1`
-
 export const domain = () => {
   console.log("now", process.env)
   console.log("state:", state())
