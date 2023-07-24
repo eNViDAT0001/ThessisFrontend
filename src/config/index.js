b0b491d164544a9658f36e1ceb8d23e32cdd8db4
eNViDAT <ngvidat@gmail.com>
2023-07-25 03:46:11 +0700

    x

--- src/config/index.js
+++ src/config/index.js
@@ -7,6 +7,6 @@ export const API_BASE_URL_WEBSOCKET = "ws://localhost:8082/api/v1/";
 const baseURL = host => `http://${host}:${PORT}/api/v1`
 const baseWebsocket = host => `ws://${host}:${PORT}/api/v1`
 
-export const domain = () => baseURL("thesis.id.vn");
+export const domain = () => `http://thesis.id.vn/api/v1`;
 
-export const webSocket = () => baseWebsocket("thesis.id.vn");
+export const webSocket = () => `ws://thesis.id.vn/api/v1`;
